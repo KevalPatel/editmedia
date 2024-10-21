4790fc2df504ba24cd101f3c77b8097185a5d78b
KevalPatel <keval.patel21790@yahoo.com>
2024-10-21 09:26:21 +0530

    initial 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import firebase from 'firebase/compat/app';
+import { GoogleAuthProvider } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'editmedia';
+
+  private firestore: Firestore = inject(Firestore);
+  private fireauth: AngularFireAuth = inject(AngularFireAuth);
+  currentUser: firebase.User | null = null;
+  dummy: boolean = true;
+
+  items$: Observable<any[]>;
+
+  provider = new GoogleAuthProvider();
+
+  constructor() {
+    const itemsCollection = collection(this.firestore, 'users');
+    this.items$ = collectionData(itemsCollection);
+    console.log(this.items$);
+
+    this.fireauth.authState.subscribe((user) => {
+      this.currentUser = user;
+      console.log('USER: ', user);
+    });
+  }
+
+  login() {
+    // this.provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    this.fireauth.signInWithPopup(this.provider).then((result) => {
+      var credential = result.credential;
+      var user = result.user;
+    });
+
+    // this.fireauth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.fireauth.signOut();
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyDqcbjeJBxZiJB3r9h6x34LV6Tkyn_5RmY',
+      authDomain: 'video-edit-c6a75.firebaseapp.com',
+      projectId: 'video-edit-c6a75',
+      storageBucket: 'video-edit-c6a75.appspot.com',
+      messagingSenderId: '639953258552',
+      appId: '1:639953258552:web:8a1aa8642500369469fecd',
+      measurementId: 'G-7CJEJF05FF',
+    },
+};
